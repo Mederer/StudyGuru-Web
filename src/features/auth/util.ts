7dd33678f802775e08dae0573c562d1aab962439
Mitchell Dederer <mitchelldederer@gmail.com>
2024-03-30 13:53:32 +1100

    feat: progress

--- src/features/auth/util.ts
+++ src/features/auth/util.ts
@@ -1,7 +1,9 @@
-import {User} from "oidc-client-ts";
+import { User } from "oidc-client-ts";
 
 export function getUser() {
-    const oidcStorage = sessionStorage.getItem("oidc.user:https://studyguruorg.b2clogin.com/studyguruorg.onmicrosoft.com/B2C_1_SignInSignUp/v2.0/:53f95087-8c02-4dbc-a3ed-18ba01e77a8f")
+    const oidcStorage = sessionStorage.getItem(
+        "oidc.user:https://studyguruorg.b2clogin.com/studyguruorg.onmicrosoft.com/B2C_1_SignInSignUp/v2.0/:53f95087-8c02-4dbc-a3ed-18ba01e77a8f",
+    );
     if (!oidcStorage) {
         return null;
     }
@@ -12,5 +14,5 @@ export function getUser() {
 export const signoutParams = {
     extraQueryParams: {
         post_logout_redirect_uri: "http://localhost:5173/",
-    }
-}
+    },
+};
